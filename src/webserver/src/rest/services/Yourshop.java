8ef41498520c973516cac0112a139c1810cbbd8c
Arzoc <arz0carz0c2@gmail.com>
2019-05-07 19:59:11 +0200

    sqlite connection established

--- src/webserver/src/rest/services/Yourshop.java
+++ src/webserver/src/rest/services/Yourshop.java
@@ -1,55 +1,51 @@
 package rest.services;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.glassfish.jersey.server.mvc.Viewable;
 
+import java.net.URI;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-@Path("/Yourshop")
+@Path("/")
 public class Yourshop {
 
 	private Connection connection = null;
-	private final Logger logger = LogManager.getLogger(Yourshop.class);
-	private String db_filename = "yourshop.db";
+	private String rootpath = "/Yourshop";
+	private String db_filename = "C:\\Users\\Arzoc\\Desktop\\uni\\webanwendungen2\\git\\Webanwendungen_2_Praktikum_Shop\\src\\database\\yourshop.db";
 	
 	public Yourshop() {
 		super();
-		System.out.println("\n\n\n\nconst\n\n\n\n");
-		this.logger.error("here");
 	}
 	
 	private Connection getSQLConnection() throws Exception {
-		Connection conn = DriverManager.getConnection("jdbs:sqlite:" + this.db_filename);
+		Class.forName("org.sqlite.JDBC");
+		Connection conn = DriverManager.getConnection("jdbc:sqlite:" + this.db_filename);
 		return conn;
 	}
 	
-	@GET
-	@Produces(MediaType.TEXT_HTML)
-	@Path("index")
-	public Response getIndex() {
-		System.out.println("\n\nhere\n\n");
+	@POST
+	@Path("/login")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response login(@FormParam("email") String email, @FormParam("password") String passwordHash) {
 		if (this.connection == null) {
 			try {
 				this.connection = this.getSQLConnection();
 			} catch (Exception e) {
-				this.logger.error("Failed to get sql connection: " + e.toString());
-				return Response
-						.status(Response.Status.INTERNAL_SERVER_ERROR)
-						.entity(new Viewable("/internal_server_error.html"))
-						.build();
+				System.out.println(e.toString());
+				return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
 			}
 		}
-		return Response
-				.status(Response.Status.OK)
-				.entity(new Viewable("/index.html"))
-				.build();
+		PreparedStatement prepStatement = this.connection.prepareStatement("select ")
 	}
 	
 }
