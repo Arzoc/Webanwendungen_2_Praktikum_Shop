cf8b4e1fb2c4430a836cd6ace5d58088ad2a4f57
arzoc <arzoc@beta-2-win>
2019-06-18 10:00:47 +0200

    user configuration for sqlfilepath -> will show merge error, keep own version

---
+++ src/webserver/src/other/UserDependableConfiguration.java
@@ -0,0 +1,7 @@
+package other;
+
+public final class UserDependableConfiguration {
+	
+	public static final String sqlfilename = "C:\\Users\\Arzoc\\Desktop\\uni\\webanwendungen2\\git\\Webanwendungen_2_Praktikum_Shop\\src\\database\\yourshop.db";
+	
+}
