7c6fae94a2f16efb36cdd15e3367964374f52c7d
arzoc <arzoc@beta-2-win>
2019-06-18 17:07:25 +0200

    small fixes

--- src/webserver/src/other/UserDependableConfiguration.java
+++ src/webserver/src/other/UserDependableConfiguration.java
@@ -1,7 +1,11 @@
 package other;
 
+/*
+ * This will cause a merge conflict -> just keep the local version.
+ * At some point this will be in .gitignore.....	
+ */
+
 public final class UserDependableConfiguration {
 	
 	public static final String sqlfilename = "C:\\Users\\Arzoc\\Desktop\\uni\\webanwendungen2\\git\\Webanwendungen_2_Praktikum_Shop\\src\\database\\yourshop.db";
-	
 }
