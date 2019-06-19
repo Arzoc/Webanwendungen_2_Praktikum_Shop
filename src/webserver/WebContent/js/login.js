4fc2361c97ea5bf2b8f5f9d193af22b1fa8056d0
whoami0I0I <philipp@laptop>
2019-06-19 12:54:47 +0200

    Set jwt in local storage

--- src/webserver/WebContent/js/login.js
+++ src/webserver/WebContent/js/login.js
@@ -4,19 +4,22 @@ $( "#login button").click(function( event ) {
  var p_w= document.querySelector('input[name="password"]').value;
   console.log($("#login button"));
  	$.ajax({
- type: "POST", 
- url: "/Yourshop/rest/users/login",
-  data: { email: na_me, password: p_w
-    
-  },
-  success: function( result, textStatus ) {
-	  console.log(textStatus)
-	  if (textStatus==401){
-    	console.log(textStatus)
-    }},
-    error: function( text, textStatus ) {
-  
-    	
+ 		type: "POST", 
+ 		url: "/Yourshop/rest/users/login",
+ 		data: { email: na_me, password: p_w},
+ 		//if we have token send this Token
+ 		beforeSend: function(xhr){
+ 			if (localStorage.token) {
+ 			xhr.setRequestHeader('Authorization', 'Bearer' + localStorage.token);
+ 			}
+ 		},
+ 		success: function( result, textStatus ) {
+		  //Set JWT token
+		  localStorage.token=result.token;
+		  console.log(result.token);
+		  alert( "Eingeloggt");
+ 												},
+ 		error: function( text, textStatus ) {
     	console.log(text.status);
   	  	if (text.status==401){
   		$( "#login" ).append( "<p><strong>Falsche Anmeldedaten</strong></p>" );
